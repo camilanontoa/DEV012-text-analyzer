6a8b05ecca199efad30a1ed547ace8e38eb37e01
camilanontoa <camilam.1024@gmail.com>
2023-10-17 14:42:25 -0500

    textanalyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,44 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+
+    const filteredWords = words.filter((word) => word.trim() !== "");
+    return filteredWords.length;
   },
   getCharacterCount: (text) => {
-    return  "Recuento de cáracteres" +text.length //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const cleanText = text.replace(/[ \.,;!?]/g, "");
+    return cleanText.length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    const filteredWords = words.map((palabra) =>
+      palabra.replace(/[.,;!?]/g, "")
+    );
+    return filteredWords.length > 0
+      ? filteredWords.reduce(
+          (acumulador, palabra) => acumulador + palabra.length,
+          0
+        ) / filteredWords.length
+      : 0;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    return text.match(/\d+\.\d+|\d+/g).length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const foundNumbers = text.match(/\d+\.\d+|\d+/g);
+    return foundNumbers.reduce(
+      (acumulador, numero) => acumulador + parseFloat(numero),
+      0
+    );
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import analyzer from './analyzer.js';
 
-const boton = document.getElementById("reset-button");
+const boton = document.getElementById('reset-button');
 const textarea = document.querySelector('textarea[name="user-input"]');
-const segundaMetrica = document.querySelector('li.categoria:nth-child(1)');
+const metrica1 = document.querySelector("li.categoria:nth-child(1) span");
+const metrica2 = document.querySelector("li.categoria:nth-child(2) span");
+const metrica3 = document.querySelector("li.categoria:nth-child(3) span");
+const metrica4 = document.querySelector("li.categoria:nth-child(4) span");
+const metrica5 = document.querySelector("li.categoria:nth-child(5) span");
+const metrica6 = document.querySelector("li.categoria:nth-child(6) span");
 
-boton.addEventListener('click',function (){
-    if(textarea.value===''){
-        alert("Ya está vacío");
-    }
-    else{textarea=''}
-}
-)
-textarea.addEventListener('input', function(){
-    const textAnalyzer = analyzer.getCharacterCount(textarea.value);
-    segundaMetrica.textContent=textAnalyzer;
-})
-
-
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+boton.addEventListener("click", function () {
+  if (textarea.value === "") {
+    alert("Ya está vacío");
+  } else {
+    textarea.value = "";
+  }
+});
+textarea.addEventListener("input", function () {
+  const puntuactionMarks = /^[.,\/#!$%\^&\*;:{}=\-_`~()]+$/.test(
+    textarea.value
+  );
+  if (textarea.value === "" || puntuactionMarks) {
+    metrica1.textContent = "0";
+    metrica2.textContent = "0";
+    metrica3.textContent = "0";
+    metrica4.textContent = "0";
+    metrica5.textContent = "0";
+    metrica6.textContent = "0";
+  } else {
+    metrica1.textContent = analyzer.getWordCount(textarea.value);
+    metrica2.textContent = analyzer.getCharacterCount(textarea.value);
+    metrica3.textContent = analyzer.getCharacterCountExcludingSpaces(
+      textarea.value
+    );
+    metrica4.textContent = analyzer.getAverageWordLength(textarea.value);
+    metrica5.textContent = analyzer.getNumberCount(textarea.value);
+    metrica6.textContent = analyzer.getNumberSum(textarea.value);
+  }
+});
